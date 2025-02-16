3defabdbffb8d40b09b2d970a23c257ff9e9aa08
obogrelova <on.obogrelova@gmail.com>
2025-02-16 10:20:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram.types import Message, FSInputFile
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 import requests
+import random
 
 from config import TOKEN
 import sqlite3
@@ -90,7 +91,64 @@ async def exchange_rates(message: Message):
     except:
         await message.answer('Произошла ошибка')
 
+@dp.message(F.text == 'Советы по экономии')
+async def send_tips(message:Message):
+    tips = [
+        'Совет 1: Ведите бюджет и следите за своими расходами.',
+        'Совет 2: Контролируйте свои расходы и избегайте импульсивных покупок.',
+        'Совет 3: Откладывайте часть доходов на сбережения.',
+        'Совет 4: Обучайтесь основам финансовой грамотности.',
+        'Совет 5: Используйте кредитные карты с умом и вовремя погашайте задолженность.',
+        'Совет 6: Планируйте крупные покупки заранее и старайтесь избегать долгов.'
+    ]
+    tip = random.choice(tips)
+    await message.answer(tip)
 
+@dp.message(F.text == 'Личные финансы')
+async def finances(massage: Message, state: FSMContext):
+    await state.set_state(FinancesForm.category1)
+    await message.reply('Введите первую категорию расходов:')
+
+@dp.message(FinancesForm.category1)
+async def finances(massage: Message, state: FSMContext):
+    await state.update_data(category1 = message.text)
+    await state.set_state(FinancesForm.expenses1)
+    await message.reply('Введите расходы для категории 1:')
+
+@dp.message(FinancesForm.expenses1)
+async def finances(massage: Message, state: FSMContext):
+    await state.update_data(expenses1 = float(message.text))
+    await state.set_state(FinancesForm.category2)
+    await message.reply('Введите вторую категорию расходов:')
+
+@dp.message(FinancesForm.category2)
+async def finances(massage: Message, state: FSMContext):
+    await state.update_data(category2 = message.text)
+    await state.set_state(FinancesForm.expenses2)
+    await message.reply('Введите расходы для категории 2:')
+
+@dp.message(FinancesForm.expenses2)
+async def finances(massage: Message, state: FSMContext):
+    await state.update_data(expenses2 = float(message.text))
+    await state.set_state(FinancesForm.category3)
+    await message.reply('Введите третью категорию расходов:')
+
+@dp.message(FinancesForm.category3)
+async def finances(massage: Message, state: FSMContext):
+    await state.update_data(category3 = message.text)
+    await state.set_state(FinancesForm.expenses3)
+    await message.reply('Введите расходы для категории 3:')
+
+@dp.message(FinancesForm.expenses3)
+async def finances(massage: Message, state: FSMContext):
+    data = await state.get_data()
+    telegram_id = message.from_user.id
+    cursor.execute('''UPDATE users SET category1 = ?, expenses1 = ?, category2 = ?, expenses2 = ?, category3 = ?, expenses3 = ? WHERE telegram_id = ?''',
+                   (data[category1], data[expenses1], data[category2], data[expenses2], data[category3], data[expenses3], float(message.text), telegram_id))
+    conn.commit()
+    await state.clear()
+
+    await message.answer('Категории и расходы сохранены!')
 
 
 async def main():
